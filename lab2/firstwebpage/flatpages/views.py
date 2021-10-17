2ae68d49cb4d4cac32e8d7c0a62a737e938facb4
Kondratov <yakari322@gmail.com>
2021-10-17 21:06:11 +0300

    Relocated Project to new system

---
+++ lab2/firstwebpage/flatpages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django import template
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'templates/index.html')
+
+def hello(request):
+    return HttpResponse(u'Hello world!')
